f77bda4d3a43bd4adab15905f414530f714da0af
Henry Calderon <theceintiant@gmail.com>
2020-02-06 18:19:21 -0800

    First commit halfway through the tutorial.

---
+++ TipCalculatorStarter/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  TipCalculatorStarter
+//
+//  Created by Chase Wang on 9/19/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    
+    @IBOutlet weak var inputCardView: UIView!
+    @IBOutlet weak var billAmountTextField: BillAmountTextField!
+    @IBOutlet weak var tipPercentSegmentedControl: UIStackView!
+    
+    @IBOutlet weak var outputCardView: UIView!
+    @IBOutlet weak var tipAmountTitleLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountTitleLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        billAmountTextField.calculateButtonAction = {
+            print("Work please.")
+            
+        }
+    }
+    
+    @IBAction func themeToggled(_ sender: UISwitch) {
+        if sender.isOn{
+            print("swith toggled on")
+        }else{
+            print("switch toggled off")
+        }
+    }
+    @IBAction func tipPercentChanged(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        print("reset button tapped")
+    }
+    
+    
+}
+
